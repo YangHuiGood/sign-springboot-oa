bfadb6e61fa76392f97501a69b2d64312397447d
yanghui <286222412@qq.com>
2018-11-25 17:22:22 +0800

    sign init

---
+++ src/main/java/cn/tedu/sign_springboot_oa/App.java
@@ -0,0 +1,13 @@
+package cn.tedu.sign_springboot_oa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
