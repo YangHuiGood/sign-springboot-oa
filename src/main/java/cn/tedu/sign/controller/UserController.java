3daaa9be889a76d10c503c13a8fb56d73e91d652
wujinzhi1994 <944215172@qq.com>
2018-11-29 12:22:19 +0800

    wjz

--- src/main/java/cn/tedu/sign/controller/UserController.java
+++ src/main/java/cn/tedu/sign/controller/UserController.java
@@ -1,9 +1,7 @@
 package cn.tedu.sign.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -27,9 +25,7 @@ public class UserController {
 	@RequestMapping("/login")
 	// http://localhost:8089/sign/login?userName=admin&userPassword=1
 	public String login(String userName, String userPassword) throws Exception {
-		System.out.println(userName);
 		User user = userMapper.getUserByUP(userName, userPassword);
-
 		return ObjectUtil.mapper.writeValueAsString(user);
 	}
 }
